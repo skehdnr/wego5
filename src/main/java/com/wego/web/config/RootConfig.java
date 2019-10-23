8f2d5aabbdf9ecc6ad81a66051e7ff74074d93d8
skehdnr <skehdnr1@naver.com>
2019-10-23 18:37:40 +0900

    2019-10-23

--- src/main/java/com/wego/web/config/RootConfig.java
+++ src/main/java/com/wego/web/config/RootConfig.java
@@ -5,26 +5,37 @@ import javax.sql.DataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 @Configurable
+@MapperScan(basePackages= {"com.wego.web"})
 @ComponentScan(basePackages= {"com.wego.web"})
 public class RootConfig {
  @Bean
  public DataSource dataSource() {
-  HikariConfig hikariConfig = new HikariConfig();
-  hikariConfig.setDriverClassName("com.mysql.jdbc.Driver");
-  hikariConfig.setJdbcUrl("jdbc:mysql://localhost:3306/wego?serverTimezone=UTC");
-  hikariConfig.setUsername("wego");
-  hikariConfig.setPassword("wego");
-  
-  HikariDataSource dataSource = new HikariDataSource(hikariConfig);
-  return dataSource;
+	 HikariConfig hikariConfig = new HikariConfig();
+		hikariConfig.setDriverClassName("com.mysql.jdbc.Driver");
+		hikariConfig.setJdbcUrl("jdbc:mysql://localhost:3306/wego?serverTimezone=UTC");
+		hikariConfig.setUsername("wego");
+		hikariConfig.setPassword("wego");
+		
+		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+		
+//		  DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//
+//		    dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+//		    dataSource.setUrl("jdbc:mysql://localhost:3306/wego?serverTimezone=UTC");
+//		    dataSource.setUsername("wego");
+//		    dataSource.setPassword("wego");
+
+		    return dataSource;
  }
  @Bean
  public SqlSessionFactory sqlSessionFactory() throws Exception {
