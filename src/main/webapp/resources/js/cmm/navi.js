151b5a630ee441135464385776cb82373b135f91
skehdnr <skehdnr1@naver.com>
2019-10-30 16:37:00 +0900

    2019-10-30

--- src/main/webapp/resources/js/cmm/navi.js
+++ src/main/webapp/resources/js/cmm/navi.js
@@ -3,17 +3,17 @@ navi = (()=>{
 	const WHEN_ERR = '호출하는 JS 파일을 찾지 못했습니다.'
 	let _, js, css, img, auth_js, brd_js, router_js, navi_vue_js
 	let init =x=>{
-		_ = x._
-	    js = x.js
-	    css = x.css
-	    img = x.img
+		_ = $.ctx()
+		js = $.js()
+		css = $.css()
+		img = $.img()
         auth_js = js+'/cmm/auth.js'
         navi_vue_js = js+'/vue/navi_vue.js'
         brd_js = js+'/brd/brd.js'
         router_js = js+'/cmm/router.js'
 	}
-	let onCreate=x=>{
-		init(x)
+	let onCreate=()=>{
+		init()
 		setContnetView()
 		$.when(
 			$.getScript(auth_js),
@@ -48,7 +48,7 @@ navi = (()=>{
 	.appendTo('#menu_logout')
 	.click ( e=>{
 		alert('로그아웃 클릭')
-	       deleteCookie(_)
+	       deleteCookie()
 	       app.run(_)
         })
 	}
